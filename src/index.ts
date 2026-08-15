51a5a29f367863cc1addb80e6fea09e47686d5e2
maxhuntorlean-debug <maxhuntorlean@gmail.com>
2026-08-15 10:44:49 +0200

    Add configurable credentialed CORS for admin frontend

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
 import auth from './routes/auth'
 import users from './routes/users'
 import roles from './routes/roles'
 
 type Bindings = {
   DB: D1Database
+  ADMIN_ORIGIN?: string
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
+
+app.use(
+  '/api/*',
+  cors({
+    origin: (origin, c) => {
+      const allowedOrigin = c.env.ADMIN_ORIGIN
+
+      if (!allowedOrigin) {
+        return origin
+      }
+
+      return origin === allowedOrigin
+        ? origin
+        : allowedOrigin
+    },
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type'],
+    credentials: true
+  })
+)
 
 app.get('/', (c) => {
   return c.json({
